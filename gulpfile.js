ea247a6456421337f5f8a1d678ad67021cc99190
EbuDem <nautilustheexplorer@gmail.com>
2018-08-31 00:24:40 +0200

    Update workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,33 @@ var minify = require('gulp-minify');
 var htmlmin = require('gulp-htmlmin');
 
 gulp.task('html', function(){
-  return gulp.src('*.html')
+  return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('build/'))
 });
 
 gulp.task('css', function(){
-  return gulp.src('less/*.less')
+  return gulp.src('src/less/*.less')
     .pipe(less())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('css'))
 });
 
 gulp.task('js', function(){
-  return gulp.src('js/*.js')
+  return gulp.src('src/js/*.js')
     .pipe(minify())
-    .pipe(gulp.dest('build/js'))
+    .pipe(gulp.dest('js'))
 });
 
 
 gulp.task('img', function(){
-    return gulp.src('img/*')
-      .pipe(gulp.dest('build/img'))
+    return gulp.src('src/img/*')
+      .pipe(gulp.dest('img'))
 });
 
 gulp.task('fonts', function(){
-    return gulp.src('fonts/*')
-      .pipe(gulp.dest('build/fonts'))
+    return gulp.src('src/fonts/*')
+      .pipe(gulp.dest('fonts'))
 });
 
 gulp.task('default', [ 'html', 'css', 'js', 'img','fonts' ]);
